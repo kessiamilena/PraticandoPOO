c6336ea992380264ff9ceff92522d6f846882e1b
kessiamilena <kessia.milena124@yahoo.com>
2025-10-23 07:59:29 -0300

    Continuação da atividade, aplicando os conceitos de orientação a objetos.

---
+++ SOLID/AbertoFechado-O/DescontoNatal.cs
@@ -0,0 +1,7 @@
+namespace AbertoFechado_O
+{
+    public class DescontoNatal : Desconto
+    {
+        public override double Calcular(double valor) => valor * 0.10;
+    }
+}
---
+++ SOLID/AbertoFechado-O/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AbertoFechado_O;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Desconto d1 = new DescontoNatal();
+        Desconto d2 = new DescontoBlackFriday();
+
+        // :C ou .ToString("C") traz o formato monetário do sistema.
+        // C de Currency - moeda em inglês
+        Console.WriteLine($"Natal: {d1.Calcular(1000):C}");
+        Console.WriteLine($"Black Friday: {d2.Calcular(1000).ToString("C")}");
+        Console.WriteLine($"Black Friday: {d2.Calcular(2000)}");
+    }
+}
